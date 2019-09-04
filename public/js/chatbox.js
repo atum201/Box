4779349bf0fe49b149f28bd7a2d3694c94253db3
atum201 <atum201@gmail.com>
2019-09-04 15:19:34 +0700

    Update

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -248,6 +248,7 @@ var ChatBox = function(payload, state, socket) {
                 data:template.message.data,
                 type:template.message.type
             };
+            console.log(m,template.message.data[index].payload);
             var message = {header:template.header,message:m}
             sendMessage(template);
         })
